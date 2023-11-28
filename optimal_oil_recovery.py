07db5071209d3aa1ba3c902c578b1e0ab1e0e794
Sanat Nair <113861205+sanatnair@users.noreply.github.com>
2023-11-27 23:49:19 -0600

    Add files via upload

--- optimal_oil_recovery.py
+++ optimal_oil_recovery.py
@@ -1,6 +1,25 @@
 """Find the maximum price of oil that can be extracted"""
 import sys
 
+class Stack:
+    """This class defines a Stack."""
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        """Takes in an item and adds it to the top of the stack."""
+        self.stack.append(item)
+
+    def pop(self):
+        """Removes an item from the top of the stack."""
+        if not self.is_empty():
+            return self.stack.pop()
+        return None
+    def is_empty(self):
+        """Checks if a stack is empty."""
+        return len(self.stack) == 0
+
 def calculate_api_gravity(specific_gravity):
     """Calculate the specific gravity of the crude to use in finding price of oil extracted from rig"""
     api_gravity=[]
@@ -15,51 +34,69 @@ def calculate_rig_oil_price(api_gravity, standard_oil_cost):
     rig_oil_price=[]
     for i in range(len(api_gravity)):
         oil_value = api_gravity[i] * standard_oil_cost
+        oil_value = round(oil_value,2)
         rig_oil_price.append(oil_value)
     return rig_oil_price
 
 def maximize_oil_price(investment, num_rigs,rig_prices,rig_oil_price):
-    """Calculate and return the maximum price of oil possible and respective rigs"""
-    # Checking if the lengths of rig_prices and rig_oil_price lists are equal
-    if len(rig_prices) != len(rig_oil_price):
-        raise ValueError("Lengths of rig_prices and rig_oil_price lists should be equal.")
+        """Calculate and return the maximum price of oil possible and respective rigs"""
+        
+        # Checking if the lengths of rig_prices and rig_oil_price lists are equal
+        if len(rig_prices) != len(rig_oil_price):
+            raise ValueError("Lengths of rig_prices and rig_oil_price lists should be equal.")       
+        
+        # Creating a table to store the maximum values for different investments
+        v = [[0 for _ in range(investment + 1)] for _ in range(num_rigs + 1)]
+        
+        # Creating a table to track selected rigs
+        keep = [[0 for _ in range(investment + 1)] for _ in range(num_rigs + 1)]
 
-    n = num_rigs  # number of rigs
-
-    # Creating a 2D table to store the maximum values for different investments
-    dp = [[0.0 for _ in range(int(investment) + 1)] for _ in range(n + 1)]
-
-    # Creating a table to track selected rigs
-    selected_rigs = [[0 for _ in range(int(investment) + 1)] for _ in range(n + 1)]
-
-    # Filling the table 
-    for i in range(1, n + 1):
-        for j in range(1, int(investment) + 1):
-            if rig_prices[i - 1] <= j:
-                if rig_oil_price[i - 1] + dp[i - 1][j - int(rig_prices[i - 1])] >= dp[i - 1][j]:
-                    dp[i][j] = rig_oil_price[i - 1] + dp[i - 1][j - int(rig_prices[i - 1])]
-                    selected_rigs[i][j] = 1
+        # Filling the table 
+        for i in range(1, num_rigs + 1):
+            for w in range(investment + 1):
+                if rig_prices[i - 1] <= w and \
+                    rig_oil_price[i - 1] + v[i - 1][w - rig_prices[i - 1]] >= v[i - 1][w]:
+                    v[i][w] = rig_oil_price[i - 1] + v[i - 1][w - rig_prices[i - 1]]
+                    keep[i][w] = 1
                 else:
-                    dp[i][j] = dp[i - 1][j]
-            else:
-                dp[i][j] = dp[i - 1][j]
+                    v[i][w] = v[i - 1][w]
+                    keep[i][w] = 0
+        
+        # Reconstruct the selected rigs
+        k = investment
+        selected_items = []
+        selected_index = []
+        for i in range(num_rigs, 0, -1):
+            if keep[i][k] == 1:
+                selected_items.append(i)
+                selected_index.append(i-1)
+                k -= rig_prices[i - 1]
 
-    # Reconstruct the selected rigs
-    k = int(investment)
-    selected_items = []
-    for i in range(n, 0, -1):
-        if selected_rigs[i][k] == 1:
-            selected_items.append(i)
-            k -= int(rig_prices[i - 1])
+        return v[num_rigs][investment],selected_items,selected_index
 
-    return dp[n][int(investment)], selected_items
+def calculate_roi(selected_index,rig_oil_price,rig_prices):
+    # Calculate and sort ROI
+    roi_values = {}
+    for i, rig_index in enumerate(selected_index):
+        roi = rig_oil_price[rig_index] / (rig_prices[rig_index] * 100)
+        roi_values[i + 1] = roi
+
+    # Sort ROI values in ascending order
+    sorted_roi = sorted(roi_values.items(), key=lambda x: x[1])
+
+    # Use a stack to reverse the order
+    roi_stack = Stack()
+    for rig_index, roi in sorted_roi:
+        roi_stack.push((rig_index, roi))
+    return roi_stack
+
 
 def main():
     """Main function"""
     # The first line is the amount of investment in USD (in 100 million).
     line = sys.stdin.readline()
     line = line.strip()
-    investment = float(line)
+    investment = int(line)
 
     # The second line includes the number of rigs available.
     line = sys.stdin.readline()
@@ -71,7 +108,7 @@ def main():
     line = line.strip()
     rig_prices = line.split(",")
     for i in range(0, len(rig_prices)):
-        rig_prices[i] = float(rig_prices[i])
+        rig_prices[i] = int(rig_prices[i])
 
     # The fourth line is the specific gravity of the crude oil extracted by the rig and location.
     line = sys.stdin.readline()
@@ -91,18 +128,18 @@ def main():
     rig_oil_price=calculate_rig_oil_price(api_gravity,standard_oil_cost)
 
     # Return optimal solution.
-    max_oil_price,selected_rigs=maximize_oil_price(investment, num_rigs,rig_prices,rig_oil_price)
+    max_oil_price,selected_rigs,selected_index=maximize_oil_price(investment, num_rigs,rig_prices,rig_oil_price)
     print(f"The estimated maximum price of oil is ${max_oil_price:.2f} USD/bbl.")
     print(f"The selected rigs are: {selected_rigs}")
-
+    
+    # Print the optimized rigs sorted based on greatest ROI
+    roi_stack = calculate_roi(selected_index, rig_oil_price, rig_prices)
+    print("Optimized Rigs sorted based on greatest ROI:")
+    while not roi_stack.is_empty():
+        rig_index, roi = roi_stack.pop()
+        print(f"Rig {selected_rigs[rig_index - 1]}: ROI = {roi:.2%}")
     
 
 
 if __name__ == '__main__':
     main()
-
-    #TODO:
-
-    # make sure to have two data structures... currently lists and 2d lists... implement dictionary, heap, or queue/stack? maybe sort something?
-
-    # finally, check solution, and clean code
